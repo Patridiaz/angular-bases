a6efb8b71b9454276e6d3b54babc25d14119e670
Informática <informatica03@dem.local>
2024-06-26 16:19:58 -0400

    Fin de Seccion 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+
+
+export class HeroComponent {
+
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+   return `${this.name }- ${ this.age }`
+  }
+
+  changeHero(): void {
+
+    this.name = 'Hulk'
+
+  }
+
+  changeAge(): void {
+
+    this.age = 18
+
+  }
+
+  reserForm():void {
+  this.name = 'IronMan';
+  this.age = 45;
+
+  }
+
+}
